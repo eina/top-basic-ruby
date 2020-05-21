8b565795ba2373b6490e4292b4d6bf1074693c8e
Eina Onting <einers@outlook.com>
2020-05-21 16:08:50 -0700

    implement right shift + tests

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -3,7 +3,22 @@ def caesar_cipher(string, shift=-3)
 
   def shift_char(char)    
     # shift = -3, left, shift = 3, right
-    shifted_num = char.ord + @shift
+    shifted_num = char.ord + @shift    
+
+    if(char == " ")
+      shifted_num = " "
+    end
+    
+    if(shifted_num < 97)      
+      distance_from_a = 97 - shifted_num
+      shifted_num = 123 - distance_from_a
+    end 
+
+    if(shifted_num > 122)      
+      distance_from_z = 122 - shifted_num
+      shifted_num = 96 + distance_from_z.abs()
+    end 
+
     shifted_num.chr
   end
   
@@ -15,4 +30,5 @@ def caesar_cipher(string, shift=-3)
   shifted_string.join
 end
 
-caesar_cipher('this is a lowercase test')
+# caesar_cipher('this is a lowercase test')
+# caesar_cipher('b')
--- spec/caesar_spec.rb
+++ spec/caesar_spec.rb
@@ -2,14 +2,19 @@ require_relative '../caesar_cipher'
 
 describe "caesar_cipher" do
   before do
-    @encrypted = caesar_cipher('test', 5)
+    # @encrypted = caesar_cipher('test', 5)
   end
   
   it "encrypts a single letter with a left shift of 3" do
     expect(caesar_cipher("e")).to eq("b")
   end
 
-  it "returns something" do 
-    expect(@encrypted).not_to be_nil
+  it "expects characters to wrap around the alphabet" do 
+    expect(caesar_cipher("b")).to eq("y")
+    expect(caesar_cipher("y", 3)).to eq("b")
   end
+
+  # it "returns something" do 
+  #   expect(@encrypted).not_to be_nil
+  # end
 end
