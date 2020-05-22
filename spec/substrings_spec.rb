ae9f8c367928461063f399181f88104d0f1ab0c8
Eina Onting <einers@outlook.com>
2020-05-21 20:06:46 -0700

    add test for counting instances + multiple words

--- spec/substrings_spec.rb
+++ spec/substrings_spec.rb
@@ -13,6 +13,17 @@ describe "substrings" do
 
   it "should be able to find multiple substrings" do
     dictionary = ["low", "below", "weather"]
-    expect(substrings("below", dictionary)).to include("low", "below")
+    expect(substrings("below", dictionary)).to include("below", "low")
   end
+
+  it "should be able to count instances of a substring" do
+    dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+    expect(substrings("howdy", dictionary)).to include("how" => 1, "howdy" => 1)
+  end
+
+  it "should be able to handle multiple words" do
+    dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+    expect(substrings("Howdy partner, sit down! How's it going?", dictionary)).to eq({ "down" => 1, "how" => 2, "howdy" => 1,"go" => 1, "going" => 1, "it" => 2, "i" => 3, "own" => 1,"part" => 1,"partner" => 1,"sit" => 1 })
+  end
+
 end
