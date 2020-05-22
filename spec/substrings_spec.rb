4457df465b81d8f5697d795a20442538cc70c569
Eina Onting <einers@outlook.com>
2020-05-21 19:28:28 -0700

    update test to check for hash

--- spec/substrings_spec.rb
+++ spec/substrings_spec.rb
@@ -2,12 +2,12 @@ require_relative '../substrings'
 
 describe "substrings" do  
   it "should be able to find the same word in an array" do
-    dictionary = ["meh", "weather"]
-    expect(substrings("below", dictionary)).to eq("below")
+    dictionary = ["below", "weather"]
+    expect(substrings("below", dictionary)).to eq("below" => 1)
   end
 
   it "should be able to find a substring" do
     dictionary = ["low", "weather"]
-    expect(substrings("below", dictionary)).to eq("low")
+    expect(substrings("below", dictionary)).to eq("low" => 1)
   end  
 end
