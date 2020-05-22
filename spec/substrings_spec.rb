3ea7756a94d077366cfa0167c3c561ac2ebab501
Eina Onting <einers@outlook.com>
2020-05-21 19:31:36 -0700

    update test to use include + add test for multiple substring

--- spec/substrings_spec.rb
+++ spec/substrings_spec.rb
@@ -1,13 +1,18 @@
 require_relative '../substrings'
 
-describe "substrings" do  
+describe "substrings" do    
   it "should be able to find the same word in an array" do
     dictionary = ["below", "weather"]
-    expect(substrings("below", dictionary)).to eq("below" => 1)
+    expect(substrings("below", dictionary)).to include("below")
   end
 
   it "should be able to find a substring" do
     dictionary = ["low", "weather"]
-    expect(substrings("below", dictionary)).to eq("low" => 1)
+    expect(substrings("below", dictionary)).to include("low")
   end  
+
+  it "should be able to find multiple substrings" do
+    dictionary = ["low", "below", "weather"]
+    expect(substrings("below", dictionary)).to include("low", "below")
+  end
 end
