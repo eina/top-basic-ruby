6890586d86fb6f9791c05b4c0b9c7f925d05aa4f
Eina Onting <einers@outlook.com>
2020-05-25 18:51:54 -0700

    finalize tests

--- spec/stock_picker_spec.rb
+++ spec/stock_picker_spec.rb
@@ -3,7 +3,7 @@ require_relative '../stock_picker'
 describe "stock_picker" do
   it "should return the pair of days representing the best day to buy and best day to sell" do
     expect(stock_picker([12,3,6,9,15, 6,1,10])).to eq([1, 4])
-  end
+  end  
 
   it "should return an error when the best day to sell is the day you buy them" do
     expect(stock_picker([10, 8, 6, 4, 2, 0])).to eq("Sorry, looks like you should hold on to those stocks a little longer")
--- stock_picker.rb
+++ stock_picker.rb
@@ -1,11 +1,11 @@
 def stock_picker(stonks)  
   sum = 0;
-  result = [];
+  result = [];  
 
   stonks.each_with_index do |stonk, idx|    
     stonks[idx + 1..-1].each_with_index do |next_days_stonk, next_idx|
       profit = next_days_stonk - stonk      
-      if profit > sum        
+      if profit > sum
         sum = profit        
         # add two: +1 for 0 index and +1 for starting at stonks's 2nd element
         result = [idx, next_idx + 2]      
@@ -21,5 +21,4 @@ def stock_picker(stonks)
 end
 
 # p stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
-# p stock_picker([2, 4, 6, 8, 10])
 # p stock_picker([10, 8, 6, 4, 2, 0])
