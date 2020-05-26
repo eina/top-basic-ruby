e48402312cdeefa7902d681230c320a96f5c35b5
Eina Onting <einers@outlook.com>
2020-05-25 18:44:22 -0700

    implement + test edge case if first day is best day to buy

--- spec/stock_picker_spec.rb
+++ spec/stock_picker_spec.rb
@@ -4,4 +4,8 @@ describe "stock_picker" do
   it "should return the pair of days representing the best day to buy and best day to sell" do
     expect(stock_picker([12,3,6,9,15, 6,1,10])).to eq([1, 4])
   end
+
+  it "should return an error when the best day to sell is the day you buy them" do
+    expect(stock_picker([10, 8, 6, 4, 2, 0])).to eq("Sorry, looks like you should hold on to those stocks a little longer")
+  end 
 end
--- stock_picker.rb
+++ stock_picker.rb
@@ -7,17 +7,19 @@ def stock_picker(stonks)
       profit = next_days_stonk - stonk      
       if profit > sum        
         sum = profit        
-        # add two: +1 for 0 index and +1 for starting at stonk's 2nd element
-        result = [idx, next_idx + 2]
+        # add two: +1 for 0 index and +1 for starting at stonks's 2nd element
+        result = [idx, next_idx + 2]      
       end
       # puts "idx: #{idx}, next day index: #{next_idx + 2}"
-      # puts "tomorrow: #{next_days_stonk} - today: #{stonk} = #{next_days_stonk - stonk}"
+      puts "tomorrow: #{next_days_stonk} - today: #{stonk} = #{next_days_stonk - stonk}"
     end
   end
-
-  p "highest sum: #{sum}"
-  result
+  
+  # if result is empty, it means there were no good days to buy
+  result.size == 0 ? "Sorry, looks like you should hold on to those stocks a little longer" : result  
 
 end
 
-p stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
+# p stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
+# p stock_picker([2, 4, 6, 8, 10])
+# p stock_picker([10, 8, 6, 4, 2, 0])
